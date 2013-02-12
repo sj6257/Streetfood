c5b9be99c6ddef76414e5fca09f4b65d02471694
Sandeep Jagtap <sajagtap88@gmail.com>
2013-02-12 18:04:11 +0530

    Change for the Menu

--- src/com/dev/streetfood/ShopListView.java
+++ src/com/dev/streetfood/ShopListView.java
@@ -314,8 +314,9 @@ public class ShopListView extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_shop_list_view, menu);
+		getMenuInflater().inflate(R.menu.option_meu, menu);
 		return true;
+		
 	}
 	
 	
