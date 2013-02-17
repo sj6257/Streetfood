29d99f7163c02a15fea794e70dfc4e447070e9a2
Sandeep Jagtap <sajagtap88@gmail.com>
2013-02-17 19:37:53 +0530

    getInfo function introduced
    Instead of querying multiple times
    get info function introduced to get all values in single query

--- src/com/dev/streetfood/ShopDetailView.java
+++ src/com/dev/streetfood/ShopDetailView.java
@@ -1,12 +1,22 @@
 package com.dev.streetfood;
 
+import java.util.ArrayList;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -17,6 +27,7 @@ public class ShopDetailView extends Activity {
 	private static final String TAG = "ShowDetails";
 	String sql;
 	String shopName;
+	ArrayList<BookMark> result = new ArrayList<BookMark>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,6 +43,11 @@ public class ShopDetailView extends Activity {
 				  Log.e(TAG,"Device Do Not Support Action Bar"+ex.toString());
 				  
 				}
+				
+		Bundle b = getIntent().getExtras();
+		shopName=(String) b.get("itemName");
+		getInfo(shopName);	
+		setView();
 		
 		RadioGroup radioGroupActivitySelector = (RadioGroup) findViewById(R.id.radio_group_activity_selector);
 		radioGroupActivitySelector.setOnCheckedChangeListener(new OnCheckedChangeListener() 
@@ -52,37 +68,91 @@ public class ShopDetailView extends Activity {
 	        }
 	    });
 		
-		Bundle b = getIntent().getExtras();
-		shopName=(String) b.get("itemName");
 		
 		
-		TextView txtShopName = (TextView) findViewById(R.id.txtShopName);	
+		Button  btnDirections=(Button) findViewById(R.id.btn_directions);
+		btnDirections.setOnClickListener(new OnClickListener() {
+			
+			//@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				getDirections();
+			}
+		} );
+		
+		/*TextView txtShopName = (TextView) findViewById(R.id.txtShopName);	
 		txtShopName.setText(shopName);
+		
 
 		TextView txtShopInfo = (TextView) findViewById(R.id.txtShopInfo);	
-		//TextView txtRating = (TextView) findViewById(R.id.txtRating);	
-		TextView txtAddress = (TextView) findViewById(R.id.txtAddress);	
+			
+		 TextView txtAddress = (TextView) findViewById(R.id.txtAddress);	*/
 			
 		// get relevant info from db
-		sql="select ifnull(shopInfo,\'Not Available\')  from streetShopInfo where shopName=\""+shopName+"\"";
+		/*sql="select ifnull(shopInfo,\'Not Available\')  from streetShopInfo where shopName=\""+shopName+"\"";
 		StreetFoodDataBaseAdapter mDBAdapter= new StreetFoodDataBaseAdapter(this);
 		mDBAdapter.createDatabase();       
 		mDBAdapter.open();
 		String info=mDBAdapter.getSingleStringVal(sql);
 		txtShopInfo.setText(info);
 		Log.i(TAG,info);
-		
+		*/
 		
 	
-		sql="select ifnull(address1,\'Not Available\')  from streetShopInfo where shopName=\""+shopName+"\"";
+		/*sql="select ifnull(address1,\'Not Available\')  from streetShopInfo where shopName=\""+shopName+"\"";
 		String address=mDBAdapter.getSingleStringVal(sql);
 		txtAddress.setText("Address: \n"+address);
 		Log.i(TAG,"Got Address:"+address);
 		
-		mDBAdapter.close();
+		mDBAdapter.close();*/
 		
 	}
 	
+	
+	private void setView() {
+		// TODO Auto-generated method stub
+		
+		TextView txtShopName = (TextView) findViewById(R.id.txtShopName);	
+		txtShopName.setText(shopName);
+		
+
+		TextView txtShopInfo = (TextView) findViewById(R.id.txtShopInfo);	
+		txtShopInfo.setText(result.get(0).getSnippet());
+		
+		TextView txtAddress = (TextView) findViewById(R.id.txtAddress);	
+		txtAddress.setText("Address: \n"+result.get(0).getAddress());
+		
+	}
+
+
+	public void getInfo(String shopName){
+		 
+			Log.i(TAG,"gettng Dettail Info");
+		    
+			String sql="select  S.shopName shopName,IFNULL(S.shopInfo,\"Not Available\") shopInfo,ifnull(S.address1,\'Not Available\') address,S.latitude latitude,S.longitude longitude from streetShopInfo AS S where S.shopName=\""+shopName+"\"";
+			Log.i(TAG,"Creating Adapter for Fetching Data");
+			StreetFoodDataBaseAdapter mDBAdapter= new StreetFoodDataBaseAdapter(this);
+			Log.i(TAG,"Adapter Ready..");
+			Log.i(TAG,"Creating/Opening Database");
+			mDBAdapter.createDatabase();       
+			mDBAdapter.open();
+			Log.i(TAG,"Requesting info from getInfo function");
+			result=mDBAdapter.getInfoForMap(sql);
+			Log.i(TAG,"Information Retrived Passing it to SetView");			
+			mDBAdapter.close();
+			
+	}
+	
+	protected void getDirections() {
+		// TODO Auto-generated method stub
+		double dlongtd =result.get(0).getLongitude() ,dlattd=result.get(0).getLatitude();
+		Intent intent = new Intent(Intent.ACTION_VIEW, 
+			    Uri.parse("http://maps.google.com/maps?f=d&daddr="+dlattd+","+dlongtd));
+			intent.setComponent(new ComponentName("com.google.android.apps.maps", 
+			    "com.google.android.maps.MapsActivity"));
+		startActivity(intent);
+	}
+
 	public void goToListView()
 	{
 		 Intent intent = new Intent(getApplicationContext(),ShopListView.class);
@@ -104,12 +174,13 @@ public class ShopDetailView extends Activity {
 	}
 	
 	
+	
 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_shop_detail_view, menu);
+		getMenuInflater().inflate(R.menu.option_menu, menu);
 		return true;
 	}
 	
