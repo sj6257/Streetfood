67cf668bcd30ce4c9adb50397c19622dbdbf3299
Sandeep Jagtap <sajagtap88@gmail.com>
2013-04-03 22:54:01 +0530

    Added some info

--- src/com/dev/streetfood/About.java
+++ src/com/dev/streetfood/About.java
@@ -1,5 +1,6 @@
 package com.dev.streetfood;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
@@ -19,17 +20,34 @@ public class About extends Activity {
 		TextView about=(TextView)findViewById(R.id.textView1);
 		//about.setText(R.string.abouthtml);
 		
-		try
+		if(Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1)
 		{
-		ActionBar actionBar = getActionBar();
-		actionBar.hide();
-		}
-		catch (Exception ex)
-		{
-		  Log.e(TAG,"Device Do Not Support Action Bar"+ex.toString());
-		  
-		}
-	   }
+			//Action bar code for android devices with android version more than gingerbread
+			Log.i(TAG,"Build.VERSION.SDK_INT : "+Build.VERSION.SDK_INT);
+			Log.i(TAG,"Build.VERSION_CODES.GINGERBREAD_MR1: "+Build.VERSION_CODES.GINGERBREAD_MR1);
+
+			// to hide the action bar
+			try
+			{
+			ActionBar actionBar = getActionBar();
+			actionBar.hide();
+			}
+			catch (Exception ex)
+			{
+			  Log.e(TAG,"Device Do Not Support Action Bar"+ex.toString());
+			  
+			}
+			Log.i(TAG,"Hardware Option Key Present");
+
+	     }
+		else
+		 {
+			Log.i(TAG,"Android version is less than 3.0");
+			Log.i(TAG,"Build.VERSION.SDK_INT : "+Build.VERSION.SDK_INT);
+			Log.i(TAG,"Build.VERSION_CODES.GINGERBREAD_MR1: "+Build.VERSION_CODES.GINGERBREAD_MR1);
+			
+		 }
+	}
 	
 
 	@Override
